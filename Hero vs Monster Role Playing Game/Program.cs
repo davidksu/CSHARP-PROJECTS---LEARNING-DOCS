c2a2365c062eb442523fce01e0f910ace01b39a4
David Lincoln <81927821+davidksu@users.noreply.github.com>
2024-08-01 02:00:37 -0400

    Add files via upload

---
+++ Hero vs Monster Role Playing Game/Program.cs
@@ -0,0 +1,37 @@
+﻿// Hero v Monster Role Playing Game
+// David Lincoln
+
+Random random = new Random();
+
+
+String Hero = "Hero";
+String Monster = "Monster";
+
+int HeroHealth = 100;
+int MonsterHealth = 100;    
+
+int HeroAttack = random.Next(1, 15);
+int MonsterAttack = random.Next(1, 15);
+
+while (HeroHealth > 0 && MonsterHealth > 0)
+{
+    MonsterHealth -= HeroAttack;
+    HeroHealth -= MonsterAttack;
+    
+    Console.WriteLine($"{Hero} attacks {Monster} for {HeroAttack} damage. {Monster} has {MonsterHealth} health remaining.");
+    Console.WriteLine($"{Monster} attacks {Hero} for {MonsterAttack} damage. {Hero} has {HeroHealth} health remaining.");
+    
+    if (HeroHealth <= 0)
+    {
+        Console.WriteLine($"{Hero} has been defeated!");
+    }
+    else if (MonsterHealth <= 0)
+    {
+        Console.WriteLine($"{Monster} has been defeated!");
+    }
+    else
+    {
+        HeroAttack = random.Next(1, 15);
+        MonsterAttack = random.Next(1, 15);
+    }
+}
