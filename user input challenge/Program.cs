86a6ec04535ea9ab0ffb51e2672d4c6741d6e613
David Lincoln <81927821+davidksu@users.noreply.github.com>
2024-08-01 02:01:21 -0400

    Add files via upload

---
+++ user input challenge/Program.cs
@@ -0,0 +1,27 @@
+﻿// Manage user input during this challenge
+//Author: David Lincoln
+
+string? readResponse;
+bool validentry = false;
+
+Console.WriteLine(" Enter a string at least 3 characters long:");
+
+do {
+
+    readResponse =  Console.ReadLine();
+
+    if(readResponse != null) {
+
+        if(readResponse.Length >= 3 || readResponse.Length >= 2 ) {
+
+            validentry =  true;
+        }
+
+        else {
+
+            Console.WriteLine("Invalid entry. Please try again.");
+        }
+    }
+
+} while(validentry == false);
+
