5847baef35b7e5dd241ed86a8b9e1252ccf22732
David Lincoln <81927821+davidksu@users.noreply.github.com>
2024-08-01 02:01:51 -0400

    Add files via upload

---
+++ subscriptionRenewal/Program.cs
@@ -0,0 +1,34 @@
+﻿//Improve subscription renewal rate
+//Author: David Lincoln III
+
+//Description: implement a feature that would imporve the renewal rate of subscriptions.
+
+//initialization 
+ Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// code
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
